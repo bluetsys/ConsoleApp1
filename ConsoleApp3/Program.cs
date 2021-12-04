09553b2e21e358bb139f073c5e2876a35f1551d5
WooYoung <wymoon2690@gmail.com>
2021-12-04 20:52:51 +0900

    ㅉㅈ

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,7 +6,25 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            long add = 0;
+            var st = System.DateTime.Now;
+
+            for (int index = 0; index <= 100000000; index++)
+            {
+                add = add + index;
+            }
+
+            var ed = System.DateTime.Now - st;
+
+            Console.WriteLine($"MachineName - {System.Environment.MachineName}");
+            Console.WriteLine($"OSVersion - {System.Environment.OSVersion}");
+            Console.WriteLine($"UtcNow - {System.DateTime.UtcNow}");
+            Console.WriteLine(System.DateTime.UtcNow);
+            Console.WriteLine("==========================");
+            Console.WriteLine("C# 테스트");
+            Console.WriteLine(add);
+            Console.WriteLine(ed);
+            Console.WriteLine("==========================");
         }
     }
 }
