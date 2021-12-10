757ccd9caa3a5ffc4a535730e593376668c4fe99
WooYoung <wymoon2690@gmail.com>
2021-12-11 00:37:20 +0900

    22

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,10 +7,10 @@ for (int index = 0; index <= 100000000; index++)
     add = add + index;
 }
 */
-
+var ee = System.Text.Encoding.ASCII.GetBytes("111" + System.DateTime.UtcNow.ToString());
 for( int i = 0; i < 1_000_000; i++ )
 {
-    MD5HashFunc("111" + System.DateTime.UtcNow.ToString());
+    MD5HashFunc(ee);
 }
 
 var ed = System.DateTime.Now - st;
@@ -25,8 +25,15 @@ Console.WriteLine(add);
 Console.WriteLine(ed);
 Console.WriteLine("==========================");
 
+/*
 byte[] MD5HashFunc(string str)
 {
     byte[] byteArr = System.Text.Encoding.ASCII.GetBytes(str);
+    return System.Security.Cryptography.MD5.Create().ComputeHash(byteArr);
+}
+*/
+
+byte[] MD5HashFunc(byte[] byteArr)
+{
     return System.Security.Cryptography.MD5.Create().ComputeHash(byteArr);
 }
