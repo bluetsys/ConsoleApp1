7f6c1196ca0811b411e3ec3932d6abb0b6edc52c
WooYoung <wymoon2690@gmail.com>
2024-02-14 19:04:06 +0900

    작업1

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-dotnet ./ConsoleApp1/bin/Debug/net5.0/ConsoleApp1.dll
+rem dotnet ./ConsoleApp1/bin/Debug/net5.0/ConsoleApp1.dll
 docker pull wymoon2690/consoleapp1:latest
 docker run --rm wymoon2690/consoleapp1:latest
 
-# 965950d4-8554-4672-b2db-508bf3e7b690
+rem 965950d4-8554-4672-b2db-508bf3e7b690
